e09e2be0ae4f194098eff07db7a05103bcef8090
n-vgh98 <nazi.gh988@gmail.com>
2023-03-07 12:51:42 +0330

    Create User and Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=150, blank=True, verbose_name="first name")
+    last_name = models.CharField(max_length=150, blank=True, verbose_name="last name")
+    email = models.EmailField(max_length=254, blank=True, verbose_name="email address", unique=True)
+    phone_number = models.CharField(unique=True, max_length=12)
+    avatar = models.ImageField()
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __Str__(self):
+        return self.email
