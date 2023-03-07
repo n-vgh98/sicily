970c4e4d05f69b71e5ff0784d1a664c677408cb2
n-vgh98 <nazi.gh988@gmail.com>
2023-03-07 14:03:04 +0330

    Add comment class

--- coffeeshops/admin.py
+++ coffeeshops/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Coffeeshop)
+admin.site.register(Game)
 # Register your models here.
--- coffeeshops/models.py
+++ coffeeshops/models.py
@@ -5,18 +5,30 @@ from django.conf import settings
 class Coffeeshop(models.Model):
     name = models.CharField(max_length=64)
     address = models.TextField()
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     main_image = models.ImageField(upload_to='coffeeshops/', blank=True, null=True)  # TODO CREATE GALLERY
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='coffeeshop_owner', on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
     coffeeshop = models.ForeignKey(Coffeeshop, related_name='coffeeshop_games', on_delete=models.CASCADE)
-    user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='users_games')
     title = models.CharField(max_length=64)  # TODO CHOICE FIELD
     capacity = models.PositiveSmallIntegerField(default=1)
     description = models.TextField(null=True, blank=True)
     game_time = models.DateTimeField()
     status = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    coffeeshop = models.ForeignKey(Coffeeshop, related_name='coffeeshop_comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='users_comments', on_delete=models.DO_NOTHING)
+    status = models.BooleanField(default=False)#for published
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
