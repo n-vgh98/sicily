e02c7651cf82b9a9bc81a3cb75e3ff5bad3650ce
n-vgh98 <nazi.gh988@gmail.com>
2023-03-15 23:32:40 +0330

    Create login function

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import owner_dashboard, owner_profile, login
+from .views import owner_dashboard, owner_profile, user_login
 
 urlpatterns = [
     path('owner/dashboard/', owner_dashboard, name='owner_dashboard'),
     path('owner/profile', owner_profile, name='owner_profile'),
-    path('login/', login, name='login')
+    path('login/', user_login, name='user_login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 
 def owner_dashboard(request):
@@ -9,5 +11,16 @@ def owner_profile(request):
     return render(request, 'users/owner/show_profile.html')
 
 
-def login(request):
-    return render(request, 'users/authentication/authentication.html')
+def user_login(request):
+    if request.method == 'GET':
+        return render(request, 'users/authentication/authentication.html')
+    elif request.method == 'POST':
+        phone_number = request.POST['phone_number']
+        password = request.POST['password']
+
+        user = authenticate(request, phone_number=phone_number, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('owner_profile')
+        return redirect('user_login')
+
