63f515ed394aa27e96b06ad144a0eafbd9f7b1da
n-vgh98 <nazi.gh988@gmail.com>
2023-03-15 23:43:15 +0330

    Checked user.is_anoymous in login

--- users/views.py
+++ users/views.py
@@ -12,15 +12,16 @@ def owner_profile(request):
 
 
 def user_login(request):
-    if request.method == 'GET':
-        return render(request, 'users/authentication/authentication.html')
-    elif request.method == 'POST':
-        phone_number = request.POST['phone_number']
-        password = request.POST['password']
+    if request.user.is_anonymous:
+        if request.method == 'GET':
+            return render(request, 'users/authentication/authentication.html')
+        elif request.method == 'POST':
+            phone_number = request.POST['phone_number']
+            password = request.POST['password']
 
-        user = authenticate(request, phone_number=phone_number, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('owner_profile')
-        return redirect('user_login')
-
+            user = authenticate(request, phone_number=phone_number, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('owner_profile')
+            return redirect('user_login')
+    return redirect('user_login')
