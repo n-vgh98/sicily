095e47361f55f1678ad361422085d761aa27455b
n-vgh98 <nazi.gh988@gmail.com>
2023-03-15 23:03:44 +0330

    Add authentication template in to the users app

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import owner_dashboard
+from .views import owner_dashboard, owner_profile, login
+
 urlpatterns = [
-    path('owner/', owner_dashboard, name='owner_dashboard')
+    path('owner/dashboard/', owner_dashboard, name='owner_dashboard'),
+    path('owner/profile', owner_profile, name='owner_profile'),
+    path('login/', login, name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def owner_dashboard(request):
-    return render(request, 'users/owner/owner_dashboard.html')
+    return render(request, 'users/owner/owner_base.html')
+
+
+def owner_profile(request):
+    return render(request, 'users/owner/show_profile.html')
+
+
+def login(request):
+    return render(request, 'users/authentication/authentication.html')
